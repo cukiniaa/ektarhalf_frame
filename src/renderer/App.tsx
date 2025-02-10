442622cd16b0bed42d02f9aa660832ecfb8419df
cukiniaa <kasia.anna.kowalczyk@pm.me>
2025-02-10 16:07:48 +0100

    Add photos container

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import { GoFileDirectory } from 'react-icons/go';
+import { FaArrowRotateRight } from 'react-icons/fa6';
 import './App.css';
 
 function View() {
@@ -11,11 +12,11 @@ function View() {
   const handlePhotoSelected = async (
     event: React.ChangeEvent<HTMLInputElement>,
   ) => {
-    const files = event.target.files;
+    const { files } = event.target;
     if (!files || !files.length) return;
     setImage(URL.createObjectURL(files[0]));
     setImagePath(files[0].path);
-    const dir = files[0].path.match(/(.*)[\/\\]/);
+    const dir = files[0].path.match(/(.*)[/\\]/);
     setImageDir(dir ? dir[1] : null);
   };
 
@@ -31,7 +32,47 @@ function View() {
             style={{ display: 'none' }}
           />
         </label>
-        <button className="menu-item">{imageDir}</button>
+        <p className="menu-text">{imageDir}</p>
+      </div>
+
+      <div className="main-content">
+        <div className="g-container-small-photo">
+          <img src={image || undefined} alt="Main" className="g-top-photo" />
+        </div>
+        <div className="g-container">
+          <button type="button" className="side-button left-button">
+            ←
+          </button>
+          <div className="g-photo-grid">
+            <div className="g-photo-wrapper">
+              <div className="g-photo-item">
+                <img
+                  src={image || undefined}
+                  alt="Left"
+                  className="g-large-photo"
+                />
+                <button type="button" className="g-button">
+                  <FaArrowRotateRight />
+                </button>
+              </div>
+            </div>
+            <div className="g-photo-wrapper">
+              <div className="g-photo-item">
+                <img
+                  src={image || undefined}
+                  alt="Right"
+                  className="g-large-photo"
+                />
+                <button type="button" className="g-button">
+                  <FaArrowRotateRight />
+                </button>
+              </div>
+            </div>
+          </div>
+          <button type="button" className="side-button right-button">
+            →
+          </button>
+        </div>
       </div>
     </div>
   );
